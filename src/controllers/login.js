788bafc30fb137bbb83e6c6d838d8b63e3850641
rafaabennour14@gmail.com <rafaabennour14@gmail.com>
2026-03-12 13:13:37 +0000

    modified login.js

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -6,22 +6,21 @@ const submit = document.querySelector("#submitbtn");
 submit.addEventListener("click", handleSubmit);
 
 function handleSubmit(e) {
-  e.preventDefault();
-  const mailValue = mail.value;
-  const passwordValue = password.value;
-  
-  submit.textContent = "checking ...";
-  // simuler la connexion à la base de données
-  setTimeout(() => {
-    const user = findUserByMail(mailValue, passwordValue);
-    if (user) {
-      // stocker l'objet user dans le sessionStorage
-      sessionStorage.setItem("user", JSON.stringify(user));
+    e.preventDefault();
+    const mailValue = mail.value;
+    const passwordValue = password.value;
+    
+    submit.textContent = "checking ...";
+    // simulation de la connexion à la BD
+    setTimeout(() => {
+        const user = findUserByMail(mailValue, passwordValue);
+        if (user) {
+        sessionStorage.setItem("user", JSON.stringify(user));
 
-      // rediriger vers la page dashboard
-      document.location = "dashboard.html";
-    } else {
-      alert("Invalid email or password");
-    }
-  }, 1000);
+        // rediriger vers dashboard
+        document.location = "dashboard.html";
+        } else {
+        alert("Invalid email or password");
+        }
+    }, 1000);
 }
